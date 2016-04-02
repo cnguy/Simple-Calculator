f46b84422eea0c793a3cfdff85d9e467a8fbf0a8
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-04-01 18:42:04 -0700

    Added a working multiply button! Had to remove the event within the multiply parameters, similarly to the say_hi function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# SORRY FOR THIS MESSY CODE AND BAD VARIABLE NAMES AND ALL THAT. I'm just learning and having fun!
 from tkinter import *
 import tkinter as tk
 
@@ -39,11 +39,34 @@ class App(Frame):
         self.numberthingy.bind('<Key-Return>',
                                self.print_square)
 
+
+        # TWO TEXT FIELDS FOR MULTIPLYING
+        self.firstNumberField = Entry()
+        self.firstNumberField.pack()
+
+        self.firstNumberFieldContents = DoubleVar()
+        self.firstNumberFieldContents.set(0)
+        self.firstNumberField["textvariable"] = self.firstNumberFieldContents
+        self.firstNumberField.bind('<Key-Return>', self.multiply)
+
+        self.secondNumberField = Entry()
+        self.secondNumberField.pack()
+
+        self.secondNumberFieldContents = DoubleVar()
+        self.secondNumberFieldContents.set(0)
+        self.secondNumberField["textvariable"] = self.secondNumberFieldContents
+        self.secondNumberField.bind('<Key-Return>', self.multiply)
+
     def createWidgets(self):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
+
+        self.multiplyButton = tk.Button(self)
+        self.multiplyButton["text"] = "*"
+        self.multiplyButton["command"] = self.multiply
+        self.multiplyButton.pack(side="top")
 
         self.QUIT = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
@@ -58,6 +81,9 @@ class App(Frame):
 
     def say_hi(self):
         print("hi there, everyone!")
+
+    def multiply(self):
+        print(self.firstNumberFieldContents.get() * self.secondNumberFieldContents.get())
 
 root = tk.Tk()
 my_app = App(master=root)
