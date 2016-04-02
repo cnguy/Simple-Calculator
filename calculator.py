bb1e9db4cfbc0a875a23a1a89824fbc0a8122704
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-04-01 20:49:07 -0700

    Deleted all pack-related methods. Changed my layout to be based on the grid method because of all the things I read online about it. App UI looks a lot better now. I wanted the fields to be at the top, but I couldn't get it to work so I just moved the buttons to the top.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # SORRY FOR THIS MESSY CODE AND BAD VARIABLE NAMES AND ALL THAT. I'm just learning and having fun!
-from tkinter import *
+# I will refactor the code and clean up everything once I get comfortable. :]
+# For educational purposes only. Created with the amazing PyCharm!
+from tkinter import Frame, Entry, DoubleVar
 import tkinter as tk
 
 class App(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.pack()
+        # self.pack()
+        self.grid()
         self.createWidgets()
 
         # self.entrythingy = Entry()
@@ -25,69 +28,120 @@ class App(Frame):
         # self.entrythingy.bind('<Key-Return>',
         #                       self.print_contents)
 
-         # Numbers
-        self.numberthingy = Entry()
-        self.numberthingy.pack()
+        # # Numbers
+        # self.numberthingy = Entry()
+        # self.numberthingy.pack()
+        #
+        # # here is the application variable
+        # self.contents = DoubleVar()
+        # # set it to some value
+        # self.contents.set(0)
+        # # tell the entry widget to watch this variable
+        # self.numberthingy["textvariable"] = self.contents
+        #
+        # self.numberthingy.bind('<Key-Return>',
+        #                        self.print_square)
 
-        # here is the application variable
-        self.contents = DoubleVar()
-        # set it to some value
-        self.contents.set(0)
-        # tell the entry widget to watch this variable
-        self.numberthingy["textvariable"] = self.contents
-
-        self.numberthingy.bind('<Key-Return>',
-                               self.print_square)
-
-
-        # TWO TEXT FIELDS FOR MULTIPLYING
-        self.firstNumberField = Entry()
-        self.firstNumberField.pack()
-
-        self.firstNumberFieldContents = DoubleVar()
-        self.firstNumberFieldContents.set(0)
-        self.firstNumberField["textvariable"] = self.firstNumberFieldContents
-        self.firstNumberField.bind('<Key-Return>', self.multiply)
-
-        self.secondNumberField = Entry()
-        self.secondNumberField.pack()
-
-        self.secondNumberFieldContents = DoubleVar()
-        self.secondNumberFieldContents.set(0)
-        self.secondNumberField["textvariable"] = self.secondNumberFieldContents
-        self.secondNumberField.bind('<Key-Return>', self.multiply)
 
     def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        # TWO TEXT FIELDS FOR MULTIPLYING
+        self.firstNumberField = Entry()
+        # self.firstNumberField.pack()
+        self.firstNumberField.grid(row=2, column=0)
+
+        self.firstNumberFieldContents = DoubleVar()
+        self.firstNumberFieldContents.set(0.0)
+        self.firstNumberField["textvariable"] = self.firstNumberFieldContents
+
+        self.secondNumberField = Entry()
+        # self.secondNumberField.pack()
+        self.secondNumberField.grid(row=3, column=0)
+
+        self.secondNumberFieldContents = DoubleVar()
+        self.secondNumberFieldContents.set(0.0)
+        self.secondNumberField["textvariable"] = self.secondNumberFieldContents
+
+        # self.hi_there = tk.Button(self)
+        # self.hi_there["text"] = "Hello World\n(click me)"
+        # self.hi_there["command"] = self.say_hi
+        # # self.hi_there.pack(side="top")
+        # self.hi_there.grid(row=5)
+
+        self.additionButton = tk.Button(self)
+        self.additionButton.grid(row=0, column=0)
+        self.additionButton["text"] = "+"
+        self.additionButton["command"] = self.add
+        # self.additionButton.pack(side="left")
+
+        self.minusButton = tk.Button(self)
+        self.minusButton.grid(row=0, column=1)
+        self.minusButton["text"] = "-"
+        self.minusButton["command"] = self.minus
+        #self.minusButton.pack(side="left")
 
         self.multiplyButton = tk.Button(self)
+        self.multiplyButton.grid(row=0, column=2)
         self.multiplyButton["text"] = "*"
         self.multiplyButton["command"] = self.multiply
-        self.multiplyButton.pack(side="top")
+        # self.multiplyButton.pack(side="left")
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.QUIT.pack(side="bottom")
+        self.divideButton = tk.Button(self)
+        self.divideButton.grid(row=0, column=3)
+        self.divideButton["text"] = "/"
+        self.divideButton["command"] = self.divide
+        # self.divideButton.pack(side="left")
 
-    def print_contents(self, event):
-        print("hi. contents of entry is now ---->",
-              self.contents.get())
+        self.squareButton = tk.Button(self)
+        self.squareButton.grid(row=1)
+        self.squareButton["text"] = "^2"
+        self.squareButton["command"] = self.square
+        # # self.squareButton.pack(side="left")
 
-    def print_square(self, event):
-        print(self.contents.get() * self.contents.get())
+        #
+        # self.backgroundColorToBlackButton = tk.Button(self)
+        # self.backgroundColorToBlackButton["text"] = "Black"
+        # self.backgroundColorToBlackButton.bind("<Enter>", self.changeBackgroundColorToBlack)
+        # # self.backgroundColorToBlackButton.pack(side="bottom")
+        # self.backgroundColorToBlackButton.grid(row=4)
+        #
+        # # self.QUIT = tk.Button(self, text="QUIT", fg="red",
+        # #                       command=root.destroy)
+        # self.quitButton = tk.Button(self, text="quit", command=root.destroy)
+        # # self.quitButton.pack(side="bottom")
+        # self.quitButton.grid(row=5)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    # def print_contents(self, event):
+    #     print("hi. contents of entry is now ---->",
+    #           self.contents.get())
+
+    # def print_square(self, event):
+    #     print(self.contents.get() * self.contents.get())
+
+    # def say_hi(self):
+    #     print("hi there, everyone!")
+
+    def add(self):
+        print(self.firstNumberFieldContents.get() + self.secondNumberFieldContents.get())
+
+    def minus(self):
+        print(self.firstNumberFieldContents.get() - self.secondNumberFieldContents.get())
 
     def multiply(self):
         print(self.firstNumberFieldContents.get() * self.secondNumberFieldContents.get())
 
-root = tk.Tk()
-my_app = App(master=root)
-my_app.master.title("Generic App")
-my_app.master.maxsize(800, 600)
+    def divide(self):
+        print(self.firstNumberFieldContents.get() / self.secondNumberFieldContents.get())
 
-my_app.mainloop()
+    def square(self):
+        print(self.firstNumberFieldContents.get() * self.firstNumberFieldContents.get())
+
+    # def changeBackgroundColorToBlack(self, event):
+    #     self.event.widget["activeforeground"] = "black"
+
+root = tk.Tk()
+
+app = App(master=root)
+app.master.title("My Calculator")
+app.master.maxsize(480, 600)
+app.master.minsize(200, 400)
+app.mainloop()
