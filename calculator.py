8ad0705c46a50360f35cd10f8ceec27e6cd7e815
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-04-07 17:01:57 -0700

    changed a lot of the method names that I overlooked...

--- calculator.py
+++ calculator.py
@@ -11,71 +11,72 @@ class Calculator(Frame):
         self.grid()
         self.create_fields()
         self.create_button_frame()
-        self.create_basic_operands()
-        self.create_other_math_buttons()
-        self.create_clear_and_enter()
 
     def create_fields(self):
-        self.entry = Entry()
-        self.entry.grid(row=0, sticky=W)
-        self.contents = StringVar()
-        self.entry["textvariable"] = self.contents
-        self.entry.focus_set()
-        self.entry.icursor(END)
-        self.entry.bind('<Key-Return>', self.enter)
+        self.number_field = Entry()
+        self.number_field.grid(row=0, sticky=W)
+        self.contents_of_number_field = StringVar()
+        self.number_field["textvariable"] = self.contents_of_number_field
+        self.number_field.focus_set()
+        self.number_field.icursor(END)
+        self.number_field.bind('<Key-Return>', self.enter)
 
-        self.error_output = Text(height=1)
-        self.error_output.config(state=DISABLED)
-        self.error_output.grid(row=1, sticky=N + E + S + W)
+        self.error_field = Text(height=1)
+        self.error_field.config(state=DISABLED)
+        self.error_field.grid(row=1, sticky=N + E + S + W)
 
     def create_button_frame(self):
         self.button_frame = Frame(root)
         self.button_frame.grid(row=3, columnspan=5, sticky=W)
 
-    def create_basic_operands(self):
-        self.additionButton = tk.Button(self.button_frame, text="+", command=lambda: self.append_basic_operand(0))
-        self.additionButton.grid(row=3)
+        self.create_basic_operands_buttons()
+        self.create_other_math_buttons()
+        self.create_clear_and_enter_buttons()
 
-        self.minusButton = tk.Button(self.button_frame, text="-", command=lambda: self.append_basic_operand(1))
-        self.minusButton.grid(row=3, column=1, sticky=W+E)
+    def create_basic_operands_buttons(self):
+        self.addition_button = tk.Button(self.button_frame, text="+", command=lambda: self.append_basic_operand(0))
+        self.addition_button.grid(row=3)
 
-        self.multiplyButton = tk.Button(self.button_frame, text="*", command=lambda: self.append_basic_operand(2))
-        self.multiplyButton.grid(row=3, column=2)
+        self.minus_button = tk.Button(self.button_frame, text="-", command=lambda: self.append_basic_operand(1))
+        self.minus_button.grid(row=3, column=1, sticky=W + E)
 
-        self.divideButton = tk.Button(self.button_frame, text="/", command=lambda: self.append_basic_operand(3))
-        self.divideButton.grid(row=3, column=3, sticky=W+E)
+        self.multiply_button = tk.Button(self.button_frame, text="*", command=lambda: self.append_basic_operand(2))
+        self.multiply_button.grid(row=3, column=2)
+
+        self.divide_button = tk.Button(self.button_frame, text="/", command=lambda: self.append_basic_operand(3))
+        self.divide_button.grid(row=3, column=3, sticky=W + E)
 
     def create_other_math_buttons(self):
-        self.squareButton = tk.Button(self.button_frame, text="²", command=self.square)
-        self.squareButton.grid(row=4)
+        self.square_button = tk.Button(self.button_frame, text="²", command=self.square)
+        self.square_button.grid(row=4)
 
-        self.cubeButton = tk.Button(self.button_frame, text="³", command=self.cube)
-        self.cubeButton.grid(row=4, column=1)
+        self.cube_button = tk.Button(self.button_frame, text="³", command=self.cube)
+        self.cube_button.grid(row=4, column=1)
 
-        self.squareRootButton = tk.Button(self.button_frame, text="√", font="Arial 9", command=self.square_root)
-        self.squareRootButton.grid(row=4, column=2, sticky=W + E)
+        self.sqrt_button = tk.Button(self.button_frame, text="√", font="Arial 9", command=self.square_root)
+        self.sqrt_button.grid(row=4, column=2, sticky=W + E)
 
-        self.piButton = tk.Button(self.button_frame, text="π", command=self.insert_pi)
-        self.piButton.grid(row=4, column=3)
+        self.pi_button = tk.Button(self.button_frame, text="π", command=self.insert_pi)
+        self.pi_button.grid(row=4, column=3)
 
-        self.eButton = tk.Button(self.button_frame, text="e", font="Arial 9", command=self.insert_e)
-        self.eButton.grid(row=5)
+        self.e_button = tk.Button(self.button_frame, text="e", font="Arial 9", command=self.insert_e)
+        self.e_button.grid(row=5)
 
-        self.lnButton = tk.Button(self.button_frame, text="ln", font="Arial 9", command=self.ln)
-        self.lnButton.grid(row=5, column=1)
+        self.ln_button = tk.Button(self.button_frame, text="ln", font="Arial 9", command=self.ln)
+        self.ln_button.grid(row=5, column=1)
 
-        self.logTwoButton = tk.Button(self.button_frame, text="lg₂ ", font="Arial 9", command=self.log_base_two)
-        self.logTwoButton.grid(row=5, column=2)
+        self.log_base_two_button = tk.Button(self.button_frame, text="lg₂ ", font="Arial 9", command=self.log_base_two)
+        self.log_base_two_button.grid(row=5, column=2)
 
-        self.logTenButton = tk.Button(self.button_frame, text="lg₁₀", font="Arial 9", command=self.log_base_ten)
-        self.logTenButton.grid(row=5, column=3)
+        self.log_base_ten_button = tk.Button(self.button_frame, text="lg₁₀", font="Arial 9", command=self.log_base_ten)
+        self.log_base_ten_button.grid(row=5, column=3)
 
-    def create_clear_and_enter(self):
-        self.clearButton = tk.Button(self.button_frame, text="C", command=self.clear)
-        self.clearButton.grid(row=3, column=4, sticky=E)
+    def create_clear_and_enter_buttons(self):
+        self.clear_button = tk.Button(self.button_frame, text="C", command=self.clear)
+        self.clear_button.grid(row=3, column=4, sticky=E)
 
-        self.enterButton = tk.Button(self.button_frame, text="=", command=self.enter)
-        self.enterButton.grid(row=5, column=4, sticky=E)
+        self.enter_button = tk.Button(self.button_frame, text="=", command=self.enter)
+        self.enter_button.grid(row=5, column=4, sticky=E)
 
     def append_basic_operand(self, index):
         self.reset_error_output()
@@ -85,71 +86,71 @@ class Calculator(Frame):
             2: " * ",
             3: " / ",
         }
-        self.contents.set(self.contents.get() + switch.get(index))
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(self.contents_of_number_field.get() + switch.get(index))
+        self.number_field.icursor(END)
 
     def square(self):
         self.reset_error_output()
-        self.contents.set(str(pow(float(self.contents.get()), 2)) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(pow(float(self.contents_of_number_field.get()), 2)) + ' ')
+        self.number_field.icursor(END)
 
     def cube(self):
         self.reset_error_output()
-        self.contents.set(str(pow(float(self.contents.get()), 3)) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(pow(float(self.contents_of_number_field.get()), 3)) + ' ')
+        self.number_field.icursor(END)
 
     def square_root(self):
         self.reset_error_output()
-        self.contents.set(str(sqrt(float(self.contents.get()))) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(sqrt(float(self.contents_of_number_field.get()))) + ' ')
+        self.number_field.icursor(END)
 
     def insert_pi(self):
         self.reset_error_output()
-        if len(self.contents.get()) == 0:
-            self.contents.set(str(pi))
+        if len(self.contents_of_number_field.get()) == 0:
+            self.contents_of_number_field.set(str(pi))
         else:
-            self.contents.set(self.contents.get() + ' ' + str(pi))
-        self.entry.icursor(END)
+            self.contents_of_number_field.set(self.contents_of_number_field.get() + ' ' + str(pi))
+        self.number_field.icursor(END)
 
     def insert_e(self):
         self.reset_error_output()
-        if len(self.contents.get()) == 0:
-            self.contents.set(str(e))
+        if len(self.contents_of_number_field.get()) == 0:
+            self.contents_of_number_field.set(str(e))
         else:
-            self.contents.set(self.contents.get() + ' ' + str(e))
-        self.entry.icursor(END)
+            self.contents_of_number_field.set(self.contents_of_number_field.get() + ' ' + str(e))
+        self.number_field.icursor(END)
 
     def ln(self):
         self.reset_error_output()
-        self.contents.set(str(log(float(self.contents.get()))) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(log(float(self.contents_of_number_field.get()))) + ' ')
+        self.number_field.icursor(END)
 
     def log_base_two(self):
         self.reset_error_output()
-        self.contents.set(str(log(float(self.contents.get()), 2)) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(log(float(self.contents_of_number_field.get()), 2)) + ' ')
+        self.number_field.icursor(END)
 
     def log_base_ten(self):
         self.reset_error_output()
-        self.contents.set(str(log10(float(self.contents.get()))) + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(str(log10(float(self.contents_of_number_field.get()))) + ' ')
+        self.number_field.icursor(END)
 
     def clear(self):
-        self.error_output.configure(state=NORMAL)
-        self.error_output.delete(0.0, END)
-        if len(self.contents.get()) == 0:
-            self.error_output.insert(END, 'ERROR: Already cleared!')
+        self.error_field.configure(state=NORMAL)
+        self.error_field.delete(0.0, END)
+        if len(self.contents_of_number_field.get()) == 0:
+            self.error_field.insert(END, 'ERROR: Already cleared!')
         else:
-            self.contents.set('')
-        self.error_output.configure(state=DISABLED)
+            self.contents_of_number_field.set('')
+        self.error_field.configure(state=DISABLED)
 
     def enter(self, event=None):
         self.reset_error_output()
-        self.parse_str(str(self.eval_binary_expr(*(self.contents.get().split()))))
+        self.parse_str(str(self.eval_binary_expr(*(self.contents_of_number_field.get().split()))))
 
     def parse_str(self, string):
-        self.contents.set(string + ' ')
-        self.entry.icursor(END)
+        self.contents_of_number_field.set(string + ' ')
+        self.number_field.icursor(END)
 
     def get_op(self, op):
         return {
@@ -162,19 +163,19 @@ class Calculator(Frame):
     def eval_binary_expr(self, op1, op, op2):
         op1, op2 = float(op1), float(op2)
         if operator == '/' and op2 == 0:
-            self.contents.set('0')
-            self.error_output.configure(state=NORMAL)
-            self.error_output.delete(0.0, END)
-            self.error_output.insert(END, 'ERROR: Cannot divide by 0!')
-            self.error_output.configure(state=DISABLED)
+            self.contents_of_number_field.set('0')
+            self.error_field.configure(state=NORMAL)
+            self.error_field.delete(0.0, END)
+            self.error_field.insert(END, 'ERROR: Cannot divide by 0!')
+            self.error_field.configure(state=DISABLED)
             return 0
         else:
             return self.get_op(op)(op1, op2)
 
     def reset_error_output(self):
-        self.error_output.configure(state=NORMAL)
-        self.error_output.delete(0.0, END)
-        self.error_output.configure(state=DISABLED)
+        self.error_field.configure(state=NORMAL)
+        self.error_field.delete(0.0, END)
+        self.error_field.configure(state=DISABLED)
 
 root = Tk()
 app = Calculator(root)
